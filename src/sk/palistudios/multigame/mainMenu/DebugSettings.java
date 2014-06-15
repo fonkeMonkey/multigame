3a95212720906e68bff325310ee34943bb43edb9
Pali <mr.everything1@gmail.com>
2014-06-15 17:05:41 +0200

    * Rollbacked debug settings.

--- src/sk/palistudios/multigame/mainMenu/DebugSettings.java
+++ src/sk/palistudios/multigame/mainMenu/DebugSettings.java
@@ -9,21 +9,21 @@ import sk.palistudios.multigame.game.persistence.GameSharedPref;
 public class DebugSettings {
 
     /* GAME */
-    public static final int SECONDS_PER_LEVEL = 10;//15
-    public static final int SCORE_COEFICIENT = 100;//1
-    public static final int SECONDS_PER_LEVEL_TUTORIAL = 3;//10
+    public static final int SECONDS_PER_LEVEL = 15;//15
+    public static final int SCORE_COEFICIENT = 1;//1
+    public static final int SECONDS_PER_LEVEL_TUTORIAL = 10;//10
     /* Čím väčšia číslo, tým ťahšie. */
     public static final int globalDifficultyCoeficient = 7;
     public static boolean adsActivated = false;
     /* DEBUG */
-    public static boolean debugInit = true;
-    public static boolean debugFirstRun = true;
-    public static boolean unlockAllItems = true;
-    public static boolean tutorialCompleted = true;
+    public static boolean debugInit = false;
+    public static boolean debugFirstRun = false;
+    public static boolean unlockAllItems = false;
+    public static boolean tutorialCompleted = false;
     public static boolean alwaysWinner = false;
     //    final private int GAME_REFRESH_INTERVAL = 1000 / GAME_UPDATES_PER_SECOND;
 
     public static void debugInit(Context context){
-        GameSharedPref.setGameMode("Tutorial");
+//        GameSharedPref.setGameMode("Tutorial");
     }
 }
