ca0b2b43cd74eb927fb88d2fd8506b1b19182b87
Pali <mr.everything1@gmail.com>
2014-07-25 12:50:10 +0200

    + Added own Application class for the good feeling.

---
+++ src/sk/palistudios/multigame/MgApplication.java
@@ -0,0 +1,13 @@
+package sk.palistudios.multigame;
+
+import android.app.Application;
+import android.util.Log;
+
+public class MgApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i("Multigame", "Multigame started.");
+    }
+}
